3e83227698caae4566251f72e78558a008c0f0f6
dionybusker <99057959@mydavinci.nl>
2020-04-21 23:52:24 +0200

    database connectie

---
+++ includes/connection/dbcon.php
@@ -0,0 +1,22 @@
+<?php
+
+    // connectie met de database
+    function dbcon() {
+        $servername = "localhost";
+        $username = "root";
+        $password = "mysql";
+        $dbname = "planningstool";
+        $conn = null;
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // echo "Connected successfully";
+            return $conn;
+        }
+
+        catch(PDOEception $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
