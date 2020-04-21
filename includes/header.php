b6cb04f0b8770658817fa964245cb34d68886bec
dionybusker <99057959@mydavinci.nl>
2020-04-21 20:18:17 +0200

    files toegevoegd

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>B3W29O1-planningstool</title>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://kit.fontawesome.com/8eccf2802e.js" crossorigin="anonymous"></script> <!-- font awesome -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> <!-- bootstrap -->
+    </head>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    //includes
+?>
+
+<?php include("includes/header.php"); ?>
+
+    <body>
+
+<?php include("includes/footer.php"); ?>
