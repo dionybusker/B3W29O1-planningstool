b4c778d176b2bf197f1dafbe6a739fffba2b6027
dionybusker <99057959@mydavinci.nl>
2020-04-21 23:51:56 +0200

    menu in een aparte file

---
+++ includes/navigation.php
@@ -0,0 +1,15 @@
+<div class="container col-8 mx-auto border bg-white">
+    <div class="row navbar navbar-expand mb-3">
+        <div class="col-5">
+            <h1>Spelletjesdag</h1>
+        </div>
+
+        <div class="col-7">
+            <ul class="navbar-nav">
+                <li class="nav-item mx-2"><a class="nav-link btn btn-outline-dark" href="index.php">Home</a></li>
+                <li class="nav-item mx-2"><a class="nav-link btn btn-outline-dark" href="all-games.php">Alle spellen</a></li>
+                <li class="nav-item mx-2"><a class="nav-link btn btn-outline-dark" href="#">Geplande spellen</a></li>
+                <li class="nav-item mx-2"><a class="nav-link btn btn-outline-dark" href="add-planning.php">Planning maken</a></li>
+            </ul>
+        </div>
+    </div>
