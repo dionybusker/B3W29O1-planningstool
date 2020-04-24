dc2669d3663aecba062e404a32d0738f4a9489af
dionybusker <99057959@mydavinci.nl>
2020-04-24 15:03:48 +0200

    title aan a-tag met img

--- all-games.php
+++ all-games.php
@@ -1,5 +1,5 @@
 <?php
-    require("includes/functions.php");
+    require_once("includes/functions.php");
 
     $result = getAllGames();
 
@@ -7,10 +7,11 @@
 
 <?php include("includes/header.php"); ?>
 
+    <h3>Alle spellen</h3>
     <div class="row justify-content-center">
         <?php foreach ($result as $game) { ?>
             <div class="col-3 m-2 border">
-                <a href="game.php?id=<?php echo $game['id'] ?>">
+                <a href="game.php?id=<?php echo $game['id'] ?>" title="Bekijk meer informatie over <?php echo $game['name'] ?>">
                     <img class="col-8 my-2 card-img-top" src="img/<?php echo $game['image'] ?>" alt="<?php echo $game['name'] ?>">
                 </a>
                 <h4 class="card-title"><?php echo $game['name'] ?></h4>
@@ -19,7 +20,7 @@
                     Duur van het spel: <?php echo $game['play_minutes'] ?> minuten <br>
                     Duur van de uitleg: <?php echo $game['explain_minutes'] ?> minuten
                 </p>
-                <p class="col-12 mt-2"><a class="btn btn-danger rounded-0" data-toggle="collapse" href="#game<?php echo $game['id'] ?>">Lees meer &hellip;</a></p>
+                <p class="col-12 mt-2"><a class="btn btn-info" data-toggle="collapse" href="#game<?php echo $game['id'] ?>">Lees meer &hellip;</a></p>
             </div>
         <?php } ?>
     </div>
