8cf72a317428ca4ff6ba2eb523dab9ddf3a74735
dionybusker <99057959@mydavinci.nl>
2020-04-24 12:50:06 +0200

    margin toegevoegd

--- add-planning.php
+++ add-planning.php
@@ -18,27 +18,27 @@
                     <select class="col-6" name="games">
                         <option>Selecteer een spel</option>
                         <?php foreach ($result as $game) { ?>
-                            <option value="<?php echo $game['id']; ?>"><?php echo $game['name']; ?></option>
+                            <option value="<?php echo $game['name'] ?>"><?php echo $game['name'] ?></option>
                         <?php } ?>
                     </select>
                 </div>
                 
-                <div class="row col-8">
-                    <label class="col-2" for="leader">Uitlegger:</label>
+                <div class="row col-8 my-3">
+                    <label class="col-2" for="leader">Spelleider:</label>
                     <input class="col-6" type="text" name="leader" placeholder="Wie legt het spel uit?">
                 </div>
 
-                <div class="row col-8">
+                <div class="row col-8 my-3">
                     <label class="col-2" for="players">Spelers:</label>
                     <input class="col-6" type="text" name="players" placeholder="Wie spelen er mee?"> <br>
                 </div>
 
-                <div class="row col-8">
+                <div class="row col-8 my-3">
                     <label class="col-2" for="time">Starttijd:</label>
                     <input class="col-6" type="time" name="time">
                 </div>
 
-                <input type="submit" value="Maak planning">
+                <input class="btn btn-info my-3" type="submit" value="Maak planning">
             </form>
         </p>
     </div>
