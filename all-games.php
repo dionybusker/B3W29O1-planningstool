6ca09f92399112cb08bf451d75a102d1c69b81aa
dionybusker <99057959@mydavinci.nl>
2020-04-24 12:56:26 +0200

    puntkomma's weggehaald die eigenlijk onnodig zijn

--- all-games.php
+++ all-games.php
@@ -10,16 +10,16 @@
     <div class="row justify-content-center">
         <?php foreach ($result as $game) { ?>
             <div class="col-3 m-2 border">
-                <a href="game.php?id=<?php echo $game['id']; ?>">
-                    <img class="col-8 my-2 card-img-top" src="img/<?php echo $game['image']; ?>" alt="<?php echo $game['name']; ?>">
+                <a href="game.php?id=<?php echo $game['id'] ?>">
+                    <img class="col-8 my-2 card-img-top" src="img/<?php echo $game['image'] ?>" alt="<?php echo $game['name'] ?>">
                 </a>
-                <h4 class="card-title"><?php echo $game['name']; ?></h4>
+                <h4 class="card-title"><?php echo $game['name'] ?></h4>
                 <p id="game<?php echo $game['id']; ?>" class="collapse">
-                    Aantal spelers: <?php echo $game['min_players'].'-'.$game['max_players']; ?> <br>
-                    Duur van het spel: <?php echo $game['play_minutes']; ?> minuten <br>
-                    Duur van de uitleg: <?php echo $game['explain_minutes']; ?> minuten
+                    Aantal spelers: <?php echo $game['min_players'].'-'.$game['max_players'] ?> <br>
+                    Duur van het spel: <?php echo $game['play_minutes'] ?> minuten <br>
+                    Duur van de uitleg: <?php echo $game['explain_minutes'] ?> minuten
                 </p>
-                <p class="col-12 mt-2"><a class="btn btn-danger rounded-0" data-toggle="collapse" href="#game<?php echo $game['id']; ?>">Lees meer &hellip;</a></p>
+                <p class="col-12 mt-2"><a class="btn btn-danger rounded-0" data-toggle="collapse" href="#game<?php echo $game['id'] ?>">Lees meer &hellip;</a></p>
             </div>
         <?php } ?>
     </div>
--- update-planning-form.php
+++ update-planning-form.php
@@ -15,20 +15,20 @@
             <form action="includes/update-planning.php" method="POST">
                 <div class="row col-8">
                     <label class="col-2" for="leader">Spelleider:</label>
-                    <input class="col-6" type="text" name="leader" value="<?php echo $planning['leader']; ?>">
+                    <input class="col-6" type="text" name="leader" value="<?php echo $planning['leader'] ?>">
                 </div>
 
                 <div class="row col-8">
                     <label class="col-2" for="players">Spelers:</label>
-                    <input class="col-6" type="text" name="players" value="<?php echo $planning['players']; ?>"> <br>
+                    <input class="col-6" type="text" name="players" value="<?php echo $planning['players'] ?>"> <br>
                 </div>
 
                 <div class="row col-8">
                     <label class="col-2" for="time">Starttijd:</label>
-                    <input class="col-6" type="time" name="time" value="<?php echo $planning['time']; ?>">
+                    <input class="col-6" type="time" name="time" value="<?php echo $planning['time'] ?>">
                 </div>
 
-                <input type="number" name="id" value="<?php echo $id; ?>" hidden>
+                <input type="number" name="id" value="<?php echo $id ?>" hidden>
                 <input type="submit" name="update" value="Bewerk planning">
             </form>
         </p>
