c427ca055b21c567df0e8d38e9d4bac69938d330
dionybusker <99057959@mydavinci.nl>
2020-05-04 15:22:31 +0200

    buttons toegevoegd, eigen classes voor css toegevoegd

--- all-plannings.php
+++ all-plannings.php
@@ -8,36 +8,28 @@
 ?>
 
 <?php include("includes/header.php"); ?>
+<?php include("includes/navigation.php") ?>
 
-    <h3>Geplande spellen</h3>
+    <h3 class="text-center">Geplande spellen</h3><br>
     <div class="row justify-content-center">
         <?php foreach ($result as $planning) { ?>
-            <div class="col-3 m-2 border">
-                <img class="col-8 my-2 card-img-top" src="img/<?php echo $planning['image']; ?>" alt="<?php $planning['game'] ?>">
-                <h4><?php echo $planning['game'] ?></h4>
-                <p>
+            <div class="col-3 m-2 border img-game-container max-height">
+                <img class="col-12 my-2 img-game max-height-img" src="img/<?php echo $planning['image']; ?>" alt="<?php $planning['game'] ?>">
+
+                <div class="info-game min-width">
+                    <h4><?php echo $planning['game'] ?></h4>
+
                     Spelleider: <?php echo $planning['leader'] ?> <br>
                     Spelers: <?php echo $planning['players'] ?> <br>
                     Starttijd: <?php echo $planning['time'] ?> <br><br>
 
-                    <a class="text-decoration-none" href="update-planning-form.php?id=<?php echo $planning['id'] ?>" title="Bewerk deze planning voor <?php echo $planning['game'] ?>">
-                        <button class="btn btn-success">
-                            <i class="fas fa-edit"></i>
-                        </button>
-                    </a>
+                    <a class="btn btn-success text-decoration-none" href="update-planning-form.php?id=<?php echo $planning['id'] ?>" title="Bewerk deze planning voor <?php echo $planning['game'] ?>"><i class="fas fa-edit"></i></a>
 
-                    <a class="text-decoration-none" href="includes/delete-planning.php?id=<?php echo $planning['id'] ?>" title="Verwijder deze planning voor <?php echo $planning['game'] ?>" onClick="javascript: return confirm('Weet je zeker dat je de planning wilt verwijderen?');">
-                        <button class="btn btn-danger">
-                            <i class="fas fa-trash-alt"></i>
-                        </button>
-                    </a>
+                    <a class="btn btn-danger text-decoration-none" href="includes/delete-planning.php?id=<?php echo $planning['id'] ?>" title="Verwijder deze planning voor <?php echo $planning['game'] ?>" onClick="javascript: return confirm('Weet je zeker dat je de planning wilt verwijderen?');"><i class="fas fa-trash-alt"></i></a>
 
-                    <a class="text-decoration-none" href="planned-game.php?id=<?php echo $planning['id'] ?>" title="Bekijk meer over deze planning voor <?php echo $planning['game'] ?>">
-                        <button class="btn btn-info">
-                            <i class="fas fa-question"></i>
-                        </button>
-                    </a>
-                </p>
+                    <a class="btn btn-info text-decoration-none" href="planned-game.php?id=<?php echo $planning['id'] ?>" title="Bekijk meer over deze planning voor <?php echo $planning['game'] ?>"><i class="fas fa-question"></i></a>
+                </div>
+
             </div>
         <?php } ?>
     </div>
