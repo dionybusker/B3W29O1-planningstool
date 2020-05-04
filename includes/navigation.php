7bdafdb14aefb77dee6aedd4873ab9824741daf3
dionybusker <99057959@mydavinci.nl>
2020-05-04 15:24:07 +0200

    home (index.php) uit nav gehaald

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,12 +1,10 @@
-<div class="container col-8 mx-auto border bg-white">
     <div class="row navbar navbar-expand mb-3">
-        <div class="col-5">
+        <div class="col-6">
             <h1>Spelletjesdag</h1>
         </div>
 
-        <div class="col-7">
+        <div class="col-6">
             <ul class="navbar-nav">
-                <li class="nav-item mx-2"><a class="nav-link btn btn-outline-dark" href="index.php">Home</a></li>
                 <li class="nav-item mx-2"><a class="nav-link btn btn-outline-dark" href="all-games.php">Alle spellen</a></li>
                 <li class="nav-item mx-2"><a class="nav-link btn btn-outline-dark" href="all-plannings.php">Geplande spellen</a></li>
                 <li class="nav-item mx-2"><a class="nav-link btn btn-outline-dark" href="add-planning.php">Planning maken</a></li>
