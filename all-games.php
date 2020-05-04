cdf38681e5dba8b9de9982cb29a617bc31dfe902
dionybusker <99057959@mydavinci.nl>
2020-05-04 15:21:46 +0200

    eigen classes voor css toegevoegd

--- all-games.php
+++ all-games.php
@@ -6,21 +6,20 @@
 ?>
 
 <?php include("includes/header.php"); ?>
+<?php include("includes/navigation.php") ?>
 
-    <h3>Alle spellen</h3>
+    <h3 class="text-center">Alle spellen</h3><br>
     <div class="row justify-content-center">
         <?php foreach ($result as $game) { ?>
-            <div class="col-3 m-2 border">
-                <a href="game.php?id=<?php echo $game['id'] ?>" title="Bekijk meer informatie over <?php echo $game['name'] ?>">
-                    <img class="col-8 my-2 card-img-top" src="img/<?php echo $game['image'] ?>" alt="<?php echo $game['name'] ?>">
-                </a>
-                <h4 class="card-title"><?php echo $game['name'] ?></h4>
-                <p id="game<?php echo $game['id']; ?>" class="collapse">
-                    Aantal spelers: <?php echo $game['min_players'].'-'.$game['max_players'] ?> <br>
-                    Duur van het spel: <?php echo $game['play_minutes'] ?> minuten <br>
-                    Duur van de uitleg: <?php echo $game['explain_minutes'] ?> minuten
-                </p>
-                <p class="col-12 mt-2"><a class="btn btn-info" data-toggle="collapse" href="#game<?php echo $game['id'] ?>">Lees meer &hellip;</a></p>
+            <div class="col-3 m-2 border img-game-container max-height">
+                <img class="col-12 my-2 img-game max-height-img" src="img/<?php echo $game['image'] ?>" alt="<?php echo $game['name'] ?>">
+
+                <div class="info-game">
+                    <a class="btn btn-info" href="game.php?id=<?php echo $game['id'] ?>" title="Bekijk meer informatie over <?php echo $game['name'] ?>">MEER INFO</a>
+                    
+                </div>
+
+
             </div>
         <?php } ?>
     </div>
