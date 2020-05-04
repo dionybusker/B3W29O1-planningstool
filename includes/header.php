d9a25bf2b1708c64f46e1626eab98ab9729f5051
dionybusker <99057959@mydavinci.nl>
2020-05-04 15:23:44 +0200

    eerste div toegevoegd (uit nav file)

--- includes/header.php
+++ includes/header.php
@@ -11,6 +11,6 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     </head>
 
-    <body class="bg-secondary">
+    <body>
 
-    <?php include("includes/navigation.php") ?>
+    <div class="container col-8 mx-auto border bg-white">
